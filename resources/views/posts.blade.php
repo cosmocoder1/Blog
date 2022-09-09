a9efe19bd57afb3b1e9bbc759121aa501450b367
Nathan Lucy <cosmocoder1@gmail.com>
2022-09-09 13:11:16 -0500

    intial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>My Blog</title>
+  <link href="app.css" rel="stylesheet">
+</head>
+
+<body>
+  <h1>My First Post</h1>
+
+  <p>post content</p>
+
+</body>
+
+</html>
